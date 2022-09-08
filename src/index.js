d20fe2595d2e067f85c526bcf16bd44749700cca
modehans <monicaglezsanch@gmail.com>
2022-09-08 12:31:25 +0200

    Add styles to detail Card (template engine)

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const server = express();
 // Configuramos el servidor
 server.use(cors());
 server.use(express.json({ limit: '10mb' }));
-server.set('view engine' , 'ejs' );
+server.set('view engine', 'ejs');
 
 // Arrancamos el servidor en el puerto 3000
 const serverPort = 4000;
@@ -20,37 +20,48 @@ server.listen(serverPort, () => {
 const savedCard = [];
 // Escribimos los endpoints que queramos
 server.post('/card', (req, res) => {
-  if (req.body.palette === "" && req.body.name === "" && req.body.job === "" && req.body.email === "" && req.body.phone === "" && req.body.linkedin === "" && req.body.gitHub === "" && req.body.photo === "") {
+  if (
+    req.body.palette === '' &&
+    req.body.name === '' &&
+    req.body.job === '' &&
+    req.body.email === '' &&
+    req.body.phone === '' &&
+    req.body.linkedin === '' &&
+    req.body.gitHub === '' &&
+    req.body.photo === ''
+  ) {
     const responseError = {
       success: false,
-      error: "falta completar"
-    }
+      error: 'falta completar',
+    };
     res.json(responseError);
   } else {
     const newCard = {
       id: uuidv4(),
-      ...req.body
-
-    }
+      ...req.body,
+    };
     savedCard.push(newCard);
 
     const responseSuccess = {
       cardURL: `localhost:4000/card/${newCard.id}`,
-      success: true
+      success: true,
     };
     res.json(responseSuccess);
   }
-
-
-
-
 });
 
 server.get('/card/84821662378443174', (req, res) => {
-  res.json({ url: "https://awesome-profile-cards.herokuapp.com/card/84821662378443174" })
-})
+  res.json({
+    url: 'https://awesome-profile-cards.herokuapp.com/card/84821662378443174',
+  });
+});
 
-server.get ('/prueba', ( req, res) => { res.render( 'detailCard' , { palette : 2} )})
+server.get('/prueba', (req, res) => {
+  res.render('detailCard', { palette: 2 });
+});
 
 const staticServer = './src/public-react';
 server.use(express.static(staticServer));
+
+const staticServerCss = './src/public-css';
+server.use(express.static(staticServerCss));
