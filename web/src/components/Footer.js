b5539cfc2af56d4483f8b062ce9ae27f2051339d
modehans <monicaglezsanch@gmail.com>
2022-09-13 13:35:52 +0200

    Add credits  Web Creators

--- web/src/components/Footer.js
+++ web/src/components/Footer.js
@@ -4,7 +4,7 @@ import logoFooter from '../images/logo-adalab.png';
 const Footer = () => {
   return (
     <footer className="footer">
-      <p className="footer-title">Git Brunch &#169;2022</p>
+      <p className="footer-title"> Web Creators-Git Brunch &#169;2022</p>
       <img
         src={logoFooter}
         alt="logo Adalab"
